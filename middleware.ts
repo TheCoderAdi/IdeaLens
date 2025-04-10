d96f48788af754e7f7b2b863171d5fa814dd4015
Aditya <adityasahoo652@gmail.com>
2025-04-11 00:14:19 +0530

    remove debug logging for token in middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,6 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  console.log({ request: request.cookies.get("token") });
   const isAuthPage = request.nextUrl.pathname.startsWith("/auth");
 
   if (isAuthPage && token) {
