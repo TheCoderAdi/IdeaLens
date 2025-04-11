b460ae1b38609e41112864e4396a0cd33367a9c2
Aditya <adityasahoo652@gmail.com>
2025-04-11 23:49:14 +0530

    add logout functionality to Sidebar and adjust Dashboard layout padding

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 p-6">{children}</main>
+      <main className="flex-1 p-3 ml-64">{children}</main>
     </div>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,12 +6,29 @@ import { cn } from "@/lib/utils";
 import { routes } from "@/lib/routes";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar() {
+  const router = useRouter();
   const pathname = usePathname();
 
+  const handleLogOut = async () => {
+    try {
+      await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+        withCredentials: true,
+      });
+
+      toast.success("Logged out successfully");
+      router.push("/auth");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
   return (
-    <aside className="w-64 border-r p-4 flex flex-col text-white">
+    <aside className="fixed inset-0 w-64 border-r p-4 flex flex-col text-white">
       <h2 className="text-xl font-bold mb-6 text-center">🎬 IdeaLens</h2>
       <ScrollArea className="flex-1">
         <nav className="space-y-1">
@@ -33,6 +50,13 @@ export default function Sidebar() {
           ))}
         </nav>
       </ScrollArea>
+      <Button
+        variant="destructive"
+        className="mt-5 cursor-pointer"
+        onClick={() => handleLogOut()}
+      >
+        Logout
+      </Button>
     </aside>
   );
 }
